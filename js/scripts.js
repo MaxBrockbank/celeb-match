5cb58103fe798c36abd802663827c92bc94e7f00
Max Brockbank <maxbrockbank1999@gmail.com>
2020-10-22 15:33:09 -0700

    add conditional statement as form validation
    Co-authored-by: David Sterry <sterry.david@gmail.com>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,27 +8,32 @@ $(document).ready(function(){
     const pizza = $("input:radio[name=pizza]:checked").val();
     const results = $(".results");
     let score = 0;
+    
+    console.log(pizza);
+    if(age > 0 && gender !== null && music !== null && pizza !== undefined){
+        //Age condition
+      if(age >= 25){
+        score += 2;
+      } else{
+        score += 1;
+      }
+      //Pizza condition
+      if(pizza === "yes"){
+        score +=2;
+      } else {
+        score +=1;
+      }
 
+      if(score % 2 == 0){
+        results.text("Steve Carell");
+      } else{
+        results.text("Will Farrell");
+      }
 
-    //Age condition
-    if(age >= 25){
-      score += 2;
-    } else{
-      score += 1;
-    }
-    //Pizza condition
-    if(pizza === "yes"){
-      score +=2;
     } else {
-      score +=1;
+      results.text("Test fail");
     }
-
-    if(score % 2 == 0){
-      results.text("Steve Carell");
-    } else{
-      results.text("Will Farrell");
-    }
-
+    
   })
 
 })
