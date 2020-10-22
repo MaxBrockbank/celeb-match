cbadc9c31de9f1c9a77e313ccee0ae8d67d716fd
Max Brockbank <maxbrockbank1999@gmail.com>
2020-10-22 10:55:16 -0700

    add conditional statements to make a working quiz
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>
    Co-authored-by: David Sterry <sterry.david@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,34 @@
 $(document).ready(function(){
 
   $("#dating").submit(function(event){
+    event.preventDefault();
     const age = parseInt($("#age").val());
     const gender = $("#gender").val();
     const music = $("#music").val();
     const pizza = $("input:radio[name=pizza]:checked").val();
-    console.log(pizza);
-    event.preventDefault();
+    const results = $(".results");
+    let score = 0;
+
+
+    //Age condition
+    if(age >= 25){
+      score += 2;
+    } else{
+      score += 1;
+    }
+    //Pizza condition
+    if(pizza === "yes"){
+      score +=2;
+    } else {
+      score +=1;
+    }
+
+    if(score % 2 == 0){
+      results.text("Steve Carell");
+    } else{
+      results.text("Will Farrell");
+    }
+
   })
 
 })
